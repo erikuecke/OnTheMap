4b649ef6e3d868f384e502d5bff4589077da038e
Erik Uecke <erikuecke@gmail.com>
2017-07-11 19:10:28 -0400

    Add PinMapViewController, Link Delegate to MapView on storyboard

---
+++ OnTheMap/ListTableViewController.swift
@@ -0,0 +1,10 @@
+//
+//  ListTableViewController.swift
+//  OnTheMap
+//
+//  Created by Erik Uecke on 7/11/17.
+//  Copyright © 2017 Erik Uecke. All rights reserved.
+//
+
+import Foundation
+import UIKit
---
+++ OnTheMap/PinMapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PinMapViewController.swift
+//  OnTheMap
+//
+//  Created by Erik Uecke on 7/11/17.
+//  Copyright © 2017 Erik Uecke. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class PinMapViewController: UIViewController, MKMapViewDelegate {
+    
+}
